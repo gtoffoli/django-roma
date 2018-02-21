c9ba22935c5af4d32eef2703675d0dc4b42eeccb
mrmanco <manco@linkroma.it>
2018-02-21 16:30:15 +0100

    Added show_element in context_processors.py, used in roma_base.html

--- context_processors.py
+++ context_processors.py
@@ -6,11 +6,16 @@ def context_processor(request):
         show_map = settings.SHOW_MAPS and not request.user_agent.is_bot
     except:
         show_map = False
+    try:
+        show_element = not request.user_agent.is_bot
+    except:
+        show_element = True
     my_settings = {
         'site_name': settings.SITE_NAME,
         # 'site_url': request.META['HTTP_HOST'],
         'site_url': request.META.get('HTTP_HOST', 'www.romapaese.it'),
         'map': show_map,
+        'show_element': show_element,
         'maps_key': settings.GOOGLE_MAPS_KEY,
     }
 
