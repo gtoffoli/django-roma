07b76d7561368b30fb95f9d698759d01dff908a4
mrmanco <manco@linkroma.it>
2018-05-09 14:15:58 +0200

    Removed in front-end registration and user authentication

--- urls.py
+++ urls.py
@@ -182,13 +182,14 @@ urlpatterns += patterns('',
     url (r'^/rmppstdipn/', include('paypal.standard.ipn.urls')),
 )   
 """
-# MMR old version - urlpatterns += patterns('',
+
+"""
 urlpatterns += [
     url (r'^accounts/signup/$', roma_views.signup, name='account_signup'), # 131015 GT
     url (r'^accounts/', include('allauth.urls')),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='account/profile.html'), name='welcome',),
 ]   
-
+"""
 
 from django.views import static as django_views_static
 # MMMR old version - urlpatterns += patterns('',
